8536cd827296d3d47b3f5dceefdaec62f9cbc86d
James Turton <9107319+jnturton@users.noreply.github.com>
2024-08-20 08:05:44 +0200

    Restrict MinIO test server to localhost

--- framework/resources/Datasources/s3minio/minio/run_mn.sh
+++ framework/resources/Datasources/s3minio/minio/run_mn.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 source conf/drillTestConfig.properties
 
+# Hardcoded access key for the local-only, ephemeral MinIO test server
 export MINIO_ACCESS_KEY=ZYVR33VM12IAHETIJYF9
 export MINIO_SECRET_KEY=BjGb9WpNd74t5T7FcBlXRgUcSyq505G598T8Yxg6
 
 if [ -z "$(top -bn1 |grep minio |grep -oE '^[ ]*[0-9]+' |grep -oE '[0-9]+')" ]
 then
-  nohup ${DRILL_TEST_DATA_DIR}/Datasources/s3minio/minio/minio server ${DRILL_TEST_DATA_DIR}/Datasources/s3minio/minio/minio_data >> ~/nohup.out &
+  nohup ${DRILL_TEST_DATA_DIR}/Datasources/s3minio/minio/minio server \
+    --address "localhost:9000" \
+    ${DRILL_TEST_DATA_DIR}/Datasources/s3minio/minio/minio_data \
+    >> ~/nohup.out &
   sleep 1
 fi
